4107788e53c92f1a34c10251a068de3104a83f1d
theprap <rikymon2@hotmail.com>
2017-11-21 09:59:50 -0600

    se gregaror servicios faltantes

--- purchases/api/urls.py
+++ purchases/api/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 
-from purchases.api.views import register_purchase, get_product_rate, register_rate
+from purchases.api.views import register_purchase, get_product_rate, register_rate, view_my_purchases
 
 urlpatterns = [
     url(r'^register_purchase/$', register_purchase, name='api_register_purchase'),
 
     url(r'^register_rate/$', register_rate, name='api_register_rate'),
     url(r'^get_product_rate/(?P<product_id>\d+)/$', get_product_rate, name='api_get_product_rate'),
+    url(r'^view_my_purchases/(?P<user_id>\d+)/$', view_my_purchases, name='api_get_product_rate'),
 
 ]
